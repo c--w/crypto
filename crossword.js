940bbfb0cb4c4b31e4e894a1cf364e646b3fd4b7
c--w <zevrnc@gmail.com>
2023-07-16 17:20:59 +0200

    first commit

---
+++ crossword.js
@@ -0,0 +1,176 @@
+const N = 50;
+const border_light_color = 'var(--border-light-color)';
+var coords;
+var g_rows;
+var g_cols;
+var all_letters_arr;
+function fillBoard(words) { //instantiator object for making gameboards
+    let grid = new Array(N); //create 2 dimensional array for letter grid
+    for (var i = 0; i < N; i++) {
+        grid[i] = new Array(N);
+        for (var j = 0; j < N; j++) {
+            grid[i][j] = '';
+        }
+    }
+    let word = words[0];
+    let startx = N / 2 - 2;
+    let starty = N / 2;
+    coords = [];
+    let coord = { x: startx, y: starty, l: word.length, all: [] }
+    coords.push(coord);
+    for (let i = 0; i < word.length; i++) {
+        grid[starty][startx + i] = word[i];
+        coord.all.push({ x: startx + i, y: starty })
+    }
+    let minx = coord.x;
+    let miny = coord.y;
+    let maxx = minx + coord.l;
+    let maxy = miny;
+    for (let i = 1; i < words.length; i++) {
+        word = words[i];
+        let yf = i % 2;
+        let xf = 1 - yf
+        coord = placeWord(word, xf, yf);
+        coords.push(coord);
+        if (coord.x < minx)
+            minx = coord.x;
+        if (coord.y < miny)
+            miny = coord.y;
+        if (coord.x + coord.l * xf > maxx) 
+            maxx = coord.x + coord.l * xf;
+        if (coord.y + coord.l * yf > maxy) 
+            maxy = coord.y + coord.l * yf;
+    }
+
+    function placeWord(word, xf, yf) {
+        let best_score = -1;
+        let best_coord = { l: word.length, all: [] };
+        let NN = N - 8;
+        for (let cnt = 0; cnt < NN * NN; cnt++) {
+            let iii = (397 * cnt) % (NN * NN);
+            let x = iii % NN;
+            let y = Math.floor(iii / NN);
+            let score = 0;
+            for (let i = 0; i < word.length; i++) {
+                let c = word[i];
+                let xx = x + i * xf;
+                let yy = y + i * yf;
+                if (grid[yy][xx] == c) {
+                    if (coords.find((coord, ii) => {
+                        let yyf = ii % 2;
+                        let xxf = 1 - yyf;
+                        return yyf == yf && coord.all.find(c => c.x == xx && c.y == yy);
+                    })) { // never use chars of same oriented word
+                        score = -1;
+                        break;
+                    } else {
+                        score++;
+                    }
+                } else if (grid[y + i * yf][x + i * xf] != '') { // clash with another word
+                    score = -1;
+                    break;
+                }
+            }
+            if (score > best_score) {
+                best_score = score;
+                best_coord.x = x;
+                best_coord.y = y;
+            }
+        }
+        if (best_coord.x == 0 && best_coord.y == 0) {
+            best_coord.x = minx - best_coord.l * xf;
+            best_coord.y = miny - best_coord.l * yf;
+            console.log("Floating...")
+        }
+        for (let i = 0; i < word.length; i++) {
+            c = word[i];
+            grid[best_coord.y + i * yf][best_coord.x + i * xf] = c;
+            best_coord.all.push({ x: best_coord.x + i * xf, y: best_coord.y + i * yf })
+        }
+        return best_coord;
+    }
+
+    let rows = g_rows = maxy - miny || 1;
+    let cols = g_cols = maxx - minx || 1;
+    let grid2 = new Array(rows); 
+    $('#all_words_div').empty();
+    calculateCSS();
+    let all_letters = new Set();    
+    for (var i = 0; i < rows; i++) {
+        grid2[i] = new Array(cols);
+        for (var j = 0; j < cols; j++) {
+            let l = grid[i + miny][j + minx];;
+            grid2[i][j] = l;
+            let div = $('<div>');
+            if (l) {
+                div.addClass('full');
+                all_letters.add(l);
+                let n = Array.from(all_letters).indexOf(l);
+                div.html('<span>'+n+'<span/>');
+                div.data('l', l);
+                div.data('n', n);
+                div.attr('n', n);
+            }
+            $('#all_words_div').append(div);
+        }
+    }
+    let all_divs = $($('#all_words_div div'));
+    let color_step = Math.floor(360 / words.length);
+    coords.forEach((c, i) => {
+        let yf = i % 2;
+        let xf = 1 - yf
+        let color = "hsl(" + color_step * i + "deg 100% 50% / 0.2)";
+        c.x = c.x - minx;
+        c.y = c.y - miny;
+        for (let j = 0; j < c.l; j++) {
+            let xx = c.x + j * xf;
+            let yy = c.y + j * yf;
+            let div = $(all_divs[yy * cols + xx])
+            let bckg = $('<div class="bckg">');
+            bckg.css("background", color);
+            div.append(bckg);
+            if (i % 2) {
+                if (j == 0) {
+                    div.addClass('top');
+                    div.append('<i class="bottom">')
+                } else if (j == c.l - 1) {
+                    div.addClass('bottom')
+                    div.append('<i class="top">')
+                } else {
+                    div.addClass('vertical')
+                    div.append('<i class="bottom">')
+                    div.append('<i class="top">')
+                }
+            } else {
+                if (j == 0) {
+                    div.addClass('left')
+                    div.append('<i class="right">')
+                } else if (j == c.l - 1) {
+                    div.append('<i class="left">')
+                    div.addClass('right')
+                } else {
+                    div.addClass('horizontal')
+                    div.append('<i class="right">')
+                    div.append('<i class="left">')
+                }
+            }
+        }
+    })
+    $("#hints").css("display", "flex");
+    console.table(grid2);
+}
+
+function calculateCSS() {
+    let width = Math.floor(window.innerWidth/(g_cols+1));
+    if(width>80)
+        width = 80;
+    let margin = Math.ceil(width/32);
+    let margin2 = Math.ceil(margin/1.5);
+    //width = width - 2*margin;
+    document.querySelector(':root').style.setProperty('--main-box-margin', margin+'px');
+    document.querySelector(':root').style.setProperty('--inner-box-margin', margin2+'px');
+    document.querySelector(':root').style.setProperty('--box-border-width', margin+'px');
+    $('#all_words_div').css("grid-template-columns", "repeat(" + g_cols + ", " + width + "px)");
+    $('#all_words_div').css("grid-template-rows", "repeat(" + g_rows + ", " + width + "px)");
+    $('#all_words_div').css("font-size", width / 2 + "px");
+}
---
+++ main.js
@@ -0,0 +1,254 @@
+onload = (event) => init();
+var all_guess_words;
+var all_guess_words_arr;
+var last_time = 0;
+var total_time = 0;
+var games = 0;
+var start_time = 0;
+var seed;
+var startseed;
+var letters;
+var gamemode;
+var level;
+var last_selected;
+var hints;
+function init() {
+    let all_words_div = document.querySelector("#all_words_div");
+    all_words_div.onmousedown = (event) => handleClick(event);
+    all_words_div.ontouchstart = (event) => handleClick(event);
+    $(".key")
+        .toArray()
+        .forEach((key) => {
+            $(key).data("l", $(key).text());
+            $(key).attr("l", $(key).text());
+        });
+    $(".key").on("click", handleKeyClick);
+
+    initSeed();
+    if (!gamemode) {// try cookie
+        gamemode = Number(getCookie("gamemode"));
+        level = Number(getCookie("level"));
+    }
+    if (isNaN(gamemode)) { // try select
+        gamemode = $("#gamemode").val();
+        level = $("#level").val();
+    }
+    if (gamemode < 4)
+        gamemode = 5;
+    if (level < 1)
+        level = 1;
+    $("#gamemode").val(gamemode);
+    $("#level").val(level);
+    setCookie("gamemode", gamemode, 730);
+    setCookie("level", level, 730);
+    $("#gamemode").on("change", changeGame);
+    $("#level").on("change", changeGame);
+    changeGame();
+    window.onresize = function () {
+        if (gamemode > 7)
+            calculateCSS();
+    }
+}
+
+function changeGame() {
+    gamemode = Number($("#gamemode").val());
+    if (gamemode > 7)
+        letters = gamemode - 3;
+    else
+        letters = gamemode;
+    setCookie("gamemode", gamemode, 730);
+    level = $("#level").val();
+    setup_dw();
+    setCookie("level", level, 730);
+    last_time = 0;
+    total_time = 0;
+    games = 0;
+    start_time = 0;
+    hints = 5;
+    initKeyboard();
+    setBckg();
+    initGame();
+}
+
+function initKeyboard() {
+    if (level == 4) {
+        ["Š", "Đ", "Č", "Ć", "Ž", "NJ", "DŽ", "LJ"].forEach(l => $('[l="' + l + '"]').hide());
+        ["Q", "W", "X", 'Y'].forEach(l => $('[l="' + l + '"]').show());
+    } else {
+        ["Š", "Đ", "Č", "Ć", "Ž", "NJ", "DŽ", "LJ"].forEach(l => $('[l="' + l + '"]').show());
+        ["Q", "W", "X", 'Y'].forEach(l => $('[l="' + l + '"]').hide());
+    }
+    $("#keyboard-div").hide();;
+}
+
+function initGame() {
+    startseed = seed;
+    let seed_url;
+    seed_url = gamemode + "-" + level + "-" + startseed;
+
+    var url = window.location.origin + window.location.pathname + "#" + seed_url;
+    $("#share-url").val(url);
+    $("#seed").attr('title', startseed);
+    guess_word = getRandomWord();
+    findAllGuessWords();
+    fillBoard(all_guess_words_arr);
+    $('#all_words_div').show();
+    updateStats();
+    start_time = Date.now();
+}
+
+function findAllGuessWords() {
+    all_guess_words = new Set();
+    while(all_guess_words.size < 20) {
+        all_guess_words.add(getRandomWord());
+    }
+    all_guess_words_arr = Array.from(all_guess_words);
+    console.table(Array.from(all_guess_words));
+}
+
+var click_time = 0;
+function handleClick(event) {
+    if (event.stopPropagation) {
+        event.stopPropagation();
+        event.preventDefault();
+    }
+    if (Date.now() - click_time < 100)
+        return false;
+    click_time = Date.now();
+    let el = $(event.target);
+    if (el.hasClass('full') || el.parent().hasClass('full')) {
+        if(!el.hasClass('full'))
+            el = el.parent();
+        effect(el);
+        if(hints) {
+            if(el.hasClass('l'))
+                return;
+            let num = el.data('n'); 
+            let l = el.data('l');
+            let divs = $('#all_words_div div[n=' + num + ']'); 
+            divs.addClass('success');
+            setTimeout(() => {
+                divs.removeClass('success');
+            }, 1000)
+        
+            divs.addClass('l');
+            divs.toArray().forEach(div => { 
+                $(div).find('span')[0].innerHTML = l;
+            });
+            $($('#hints i:visible')[0]).hide();
+            hints--;
+            return;
+        }
+        $("#keyboard-div").css('display', 'flex');
+        $("#all_words_div div").removeClass('selected')
+        el.addClass('selected');
+    }
+}
+
+function handleKeyClick(event) {
+    let key = $(event.target);
+    $("#keyboard-div").hide();
+    let l = key.data('l');
+    let selected = $('#all_words_div div.selected');
+    let num = selected.data('n');
+    let divs = $('#all_words_div div[n=' + num + ']'); 
+    divs.addClass('success');
+    setTimeout(() => {
+        divs.removeClass('success');
+    }, 1000)
+    if (key.hasClass('del')) {
+        if (!isNaN(Number(selected.text())))
+            return;
+        divs.removeClass('l');
+        divs.toArray().forEach(div => $(div).find('span')[0].innerHTML = num);
+    } else {
+        let key_l = key.data('l');
+        divs.addClass('l');
+        divs.toArray().forEach(div => { 
+            $(div).find('span')[0].innerHTML = l;
+        });
+    }
+}
+
+function randomsort(a, b) {
+    return Math.random() * 2 - 1;
+}
+
+function rand() {
+    seed++;
+    let t = seed + 0x6D2B79F5;
+    t = Math.imul(t ^ t >>> 15, t | 1);
+    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+}
+
+function initSeed() {
+    if (window.location.hash) {
+        let tmp = window.location.hash.substring(1).split("-");
+        gamemode = Number(tmp[0])
+        level = Number(tmp[1])
+        seed = Number(tmp[2]);
+        if (!isNaN(seed))
+            return;
+    }
+    let now = new Date();
+    seed = now.toISOString().replaceAll("-", "").replaceAll("T", "").replaceAll(":", "").substring(2, 12);
+    seed = Number(seed + '0000');
+}
+
+
+var dw;
+function setup_dw() {
+    if (level == 1) dw = hrdict1;
+    else if (level == 2) dw = hrdict2;
+    else if (level == 3) dw = hrdict3;
+    else if (level == 4) dw = endict;
+}
+
+
+function getRandomWord(len) {
+    let filtered = dw.filter((word) => {
+        word = cdl(word);
+        if(len)
+            return word.length == len;
+        else
+            return word.length <= letters;
+    });
+    let i = Math.floor(rand() * filtered.length);
+    let word = filtered[i];
+    return cdl(word);
+}
+
+function setBckg() {
+    var color = (Math.random() * 20 + 235 << 0).toString(16) + (Math.random() * 20 + 235 << 0).toString(16) + (Math.random() * 20 + 235 << 0).toString(16);
+    var url = "https://bg.siteorigin.com/api/image?2x=0&blend=40&color=%23" + color + "&intensity=10&invert=0&noise=0&pattern=" + g_patterns[Math.floor(Math.random() * g_patterns.length)];
+    $('body').css('background-image', 'url(' + url + ')');
+}
+
+function effect(el) {
+    el.addClass('effect');
+    setTimeout((el) => el.removeClass('effect'), 100, el);
+}
+
+function updateStats() {
+    $("#games").text(games);
+    $("#last").text(last_time);
+    $("#total").text(total_time);
+    if (!games)
+        return;
+    let avg = Math.round(total_time / games);
+    $("#avg").text(avg);
+    let key = 'words' + games + '-' + gamemode + '-' + level;
+    let best = localStorage.getItem(key);
+    if (best) {
+        best = Number(best);
+        if (avg < best) {
+            best = avg;
+        }
+    } else {
+        best = avg;
+    }
+    localStorage.setItem(key, best);
+    $("#best-games").text(games);
+    $("#best").text(best);
+}
